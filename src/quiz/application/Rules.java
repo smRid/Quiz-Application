d16b88ffa46d0ea10c727d3777f1602fb8053c75
Omor Riduan <93811768+smRid@users.noreply.github.com>
2024-05-13 00:25:57 +0600

    Add files via upload

---
+++ src/quiz/application/Rules.java
@@ -0,0 +1,71 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to QuizMania");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. Users required to provide their names before they can participate in the quiz." + "<br><br>" +
+                "2. Users will have 15 sec time limit for each question to ensure timely responses and prevent users from taking too long to answer." + "<br><br>" +
+                "3. Participants need to answer a total of 10 questions within a specified time limit." + "<br><br>" +
+                "4. There are 4 options for each questions." + "<br><br>" +
+                "5. Users can also use a 50/50 lifeline to reduce the options when answering." + "<br><br>" +
+                "6. After answering all the questions, the system will provide the total score of the quiz." + "<br><br>" +
+                
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
